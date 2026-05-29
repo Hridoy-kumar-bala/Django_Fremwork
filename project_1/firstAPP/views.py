e2e6cc9dd897a94db9a225269a8a243a861ad94d
Hridoy-kumar-bala <hridoykumarbala@gmail.com>
2026-05-29 20:35:47 +0600

    Django

---
+++ project_1/firstAPP/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def courses(request):
+    return HttpResponse('this is courses page. ')
+def about(request):
+    return HttpResponse('this is about page. ')
+def firstapp(request):
+    return HttpResponse('this is FIRST APP page. ')
