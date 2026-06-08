d83189390f5362634e6bd50557b1ce9ebcbc964d
Hridoy-kumar-bala <hridoykumarbala@gmail.com>
2026-06-08 15:10:38 +0600

    module_15_5

---
+++ practices_day_2/views.py
@@ -0,0 +1,8 @@
+from  django.shortcuts import render
+from musician.models import Musician
+
+from album.models import Album
+def home(request):
+    data = Album.objects.all()
+    print("TOTAL ALBUM:", data.count())
+    return render(request, 'home.html', {'data': data})
